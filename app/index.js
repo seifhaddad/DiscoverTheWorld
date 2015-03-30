e6eaf5525f28467d9330346f89f22902a04baa6d
seifhaddad <seifhaddad45@yahoo.com>
2015-03-29 19:30:59 -0500

    background size

--- app/index.js
+++ app/index.js
@@ -19,26 +19,7 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-  // var url = 'http://api.wunderground.com/api/d8b5a108b79cf7eb/conditions/q/' + req.body.zip + '.json';
-  // request(url, function(err, response, forecast){
-  //   forecast = JSON.parse(forecast);
-  //   var temperature = forecast.current_observation.temp_f;
-  //   var color;
-  //
-  // if(temperature < 32){
-  //   color = 'blue';
-  // }else if(temperature > 32 && temperature <= 70){
-  //   color = 'green';
-  // }else if( temperature > 71 && temperature <= 80){
-  //   color = 'yellow';
-  // }else if(temperature > 80 && temperature <= 95){
-  //   color = 'orange';
-  // }else{
-  //   color = 'red';
-  // }
-  //
-  // res.render('temp', {zip:req.body.zip, temperature:temperature, color:color});
-  // });
+
 });
 
 var port = process.env.PORT || 5001;
